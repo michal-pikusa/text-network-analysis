4d368b0042a900f79ba27e01cb0f39ed89f8c4f6
Michal Pikusa <pixeluz@gmail.com>
2019-01-12 15:44:34 +0100

    added community detection and upgraded viz
    Visualization now takes into account the community and the betweenness measure to set color and size of the node.

--- tna.py
+++ tna.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 from networkx.algorithms import community
 import matplotlib.pyplot as plt
 import logging
+from random import random
 
 def load_file(filename):
-    logging.warning('Loading text')
+    logging.info('Loading text')
     with open(filename,'r') as f:
         text = f.readlines()
     return text
@@ -17,7 +18,7 @@ def preprocess(line):
     return line
 
 def create_graph(text):
-    logging.warning('Creating graph')
+    logging.info('Creating graph')
     word_list = []
     G = nx.Graph()
     pbar = tqdm(total=len(text))
@@ -41,7 +42,7 @@ def create_graph(text):
     return G
 
 def calculate_central_nodes(text_network):
-    logging.warning('Calculating centrality')
+    logging.info('Calculating centrality')
     bc = (nx.betweenness_centrality(text_network,weight='weight'))
     nx.set_node_attributes(text_network, bc, 'betweenness')
     bc_threshold = sorted(bc.values(), reverse=True)[20]
@@ -50,25 +51,41 @@ def calculate_central_nodes(text_network):
     return filtered_network
 
 def create_and_assign_communities(text_network):
-    # TODO: community assignment back to the graph
+    logging.info("Assigning communities")
     communities_generator = community.girvan_newman(text_network)
     top_level_communities = next(communities_generator)
     next_level_communities = next(communities_generator)
+    communities = {}
+    for community_list in next_level_communities:
+        for item in community_list:
+            communities[item] = next_level_communities.index(community_list)
+    return communities
 
-def draw_final_graph(text_network):
-    # TODO: graph formatting, colors, size etc.
-    logging.warning('Drawing')
+def draw_final_graph(text_network,communities):
+    logging.info('Drawing')
     pos = nx.spring_layout(text_network,scale=2)
-    colors = range(text_network.number_of_edges())
-    nx.draw(text_network,with_labels=True, font_size=8,node_color="#A0CBe2",edge_Color=colors,width=4,edge_cmap=plt.cm.Blues)
+    color_list = []
+    color_map = []
+    community_count = max(communities.values())
+    for i in range(0,community_count+1):
+        color_list.append((random(), random(), random()))
+    for node in text_network:
+        color_index = communities[node]
+        color_map.append(color_list[color_index])
+    betweenness = nx.get_node_attributes(text_network,'betweenness')
+    betweenness = [x * 10000 for x in betweenness.values()]
+    nx.draw(text_network,with_labels=True,node_size=betweenness,font_size=8,node_color=color_map,width=4,edge_cmap=plt.cm.Blues)
     plt.draw()
     plt.show()
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s')
+    logging.getLogger().setLevel(logging.INFO)
     text = load_file('pubmed.txt')
     text_network = create_graph(text)
     text_network = calculate_central_nodes(text_network)
-    draw_final_graph(text_network)
+    communities = create_and_assign_communities(text_network)
+    nx.write_gml(text_network, "text_network.gml")
+    draw_final_graph(text_network,communities)
     
     
